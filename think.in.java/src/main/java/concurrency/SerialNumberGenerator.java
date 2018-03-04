5a98e7ab64e355c22124e6cb204c19de326a50b2
erik.wang <erik.wang@ximalaya.com>
2018-03-04 23:27:18 +0800

    read some code for concurrent

--- think.in.java/src/main/java/concurrency/SerialNumberGenerator.java
+++ think.in.java/src/main/java/concurrency/SerialNumberGenerator.java
@@ -1,8 +1,9 @@
 package concurrency;//: concurrency/SerialNumberGenerator.java
 
 public class SerialNumberGenerator {
-  private static volatile int serialNumber = 0;
-  public static int nextSerialNumber() {
-    return serialNumber++; // Not thread-safe
-  }
+    private static volatile int serialNumber = 0;
+
+    public static int nextSerialNumber() {
+        return serialNumber++; // Not thread-safe   //could not skip ,only maybe slow a step.
+    }
 } ///:~
