2806eb0b1b0d37423cbac4946c747e668d5ebbd8
erik.wang <erik.wang@ximalaya.com>
2018-12-23 23:09:04 +0800

    add weakReference demo

---
+++ erik.jdk.util/src/main/java/jdk/util/reference/Salad.java
@@ -0,0 +1,15 @@
+package jdk.util.reference;
+
+import java.lang.ref.WeakReference;
+
+/**
+ * Salad class
+ * 继承WeakReference，将Apple作为弱引用。
+ * 注意到时候回收的是Apple，而不是Salad
+ *
+ */
+public class Salad extends WeakReference<Apple> {
+    public Salad(Apple apple) {
+        super(apple);
+    }
+}
--- think.in.java/src/main/java/concurrency/erik/ThreadLocalVariableDemo.java
+++ think.in.java/src/main/java/concurrency/erik/ThreadLocalVariableDemo.java
@@ -5,7 +5,7 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 /**
- * @author finley
+ *
  */
 public class ThreadLocalVariableDemo extends Thread {
 
